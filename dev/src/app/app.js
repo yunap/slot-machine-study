403266e3716d154387913411519d2d04435365b0
yunap <yunay@yahoo.com>
2016-04-04 14:03:57 -0700

    Update app.js

--- dev/src/app/app.js
+++ dev/src/app/app.js
@@ -1,7 +1,9 @@
 angular.module('sandboxapp', ['sandboxapp-templates', 'sandboxapp.home', 'sandboxapp.about', 'ui.router', 'ngAnimate'])
 .config(function($stateProvider, $urlRouterProvider) {
 	'use strict';
-
+	
+  //this is a simple ui router configuration, not that this coding chalange needs the state router
+  //however I am a fan of having states in a single page app, so setting it up here as an example
 	$urlRouterProvider.otherwise('/espresso-bar');
 
 	$stateProvider
